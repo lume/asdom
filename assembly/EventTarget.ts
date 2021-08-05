cc3c309a964daef7f22cc5c8d25f3e03cb551eb2
Joe Pea <joe@trusktr.io>
2021-08-04 23:05:27 -0700

    fix: type check in `EventTarget` `add/removeEventListener` methods was wrong

--- assembly/EventTarget.ts
+++ assembly/EventTarget.ts
@@ -11,13 +11,15 @@ export type EventCallback = (/*TODO event: Event*/) => void
 
 export class EventTarget extends Object {
 	addEventListener<T>(eventName: string, listener: T): void {
-		if (isFunction<EventCallback>(listener)) addEventListenerCallback(this.__ptr__, eventName, listener.index)
+		// TODO how to check the function matches EventCallback?
+		if (isFunction<T>(listener)) addEventListenerCallback(this.__ptr__, eventName, listener.index)
 		else if (listener instanceof EventListener) addEventListenerObject(this.__ptr__, eventName, listener.__ptr__)
 		else ERROR('addEventListener expects an EventCallback or an EventListener as the second argument.')
 	}
 
 	removeEventListener<T>(eventName: string, listener: T): void {
-		if (isFunction<EventCallback>(listener)) removeEventListenerCallback(this.__ptr__, eventName, listener.index)
+		// TODO how to check the function matches EventCallback?
+		if (isFunction<T>(listener)) removeEventListenerCallback(this.__ptr__, eventName, listener.index)
 		else if (listener instanceof EventListener) removeEventListenerObject(this.__ptr__, eventName, listener.__ptr__)
 		else ERROR('removeEventListener expects an EventCallback or an EventListener as the second argument.')
 	}
